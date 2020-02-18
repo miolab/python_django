7b1c0c0491ca3e0285a36680b38f90ccebd0c8c7
im miolab <poson_hemp@yahoo.co.jp>
2020-02-18 14:02:44 +0900

    test passed: first test (django browser start or not)

--- mysite/test_function.py
+++ mysite/test_function.py
@@ -1,2 +1,17 @@
 import pytest
-from selenium import webdriver
+from selenium.webdriver import Firefox
+from selenium.webdriver import FirefoxOptions
+
+
+options = FirefoxOptions()
+driver = Firefox(options=options)
+
+
+def test_browser_get():
+    """ red (if django YET started)
+    """
+    driver.get('http://127.0.0.1:8000/')
+    assert "Django" in driver.title
+
+
+# dviver.quit()
