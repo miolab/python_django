341b124b31ba373b439eb576f49d7237a3a57fb6
im miolab <poson_hemp@yahoo.co.jp>
2020-02-23 17:31:07 +0900

    Init migrations

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title_submit = models.CharField('Title', max_length=255)
+    # date_submit = models.DateTimeField('Date')
+    text_submit = models.TextField('Text', blank=True)
+
+    def __str__(self):
+        return self.title
