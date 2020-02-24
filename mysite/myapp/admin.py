93adc18d74e73704dcde6be4e3d7e960fdd46612
im miolab <poson_hemp@yahoo.co.jp>
2020-02-24 10:54:23 +0900

    update: display article

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from myapp.models import Article
 
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title_submit')
+    list_display_links = ('id', 'title_submit')
+
+
+admin.site.register(Article, ArticleAdmin)
